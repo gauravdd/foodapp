bdbd5fafbc78e979c83f94442a61184939d485a4
Gaurav Devdutt <gdevdutt@gdevdutt-C02W5K5LHTD6.local>
2022-11-24 10:52:21 +0530

    initial commit

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,17 @@
+import classes from './MealsSummary.module.css';
+
+const MealsSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h2>Good Food, Great Time </h2>
+      <p>
+    
+      Our chefs at WiWi make delicious food selections every week - you pick, we cook and deliver.
+      </p>
+    </section>
+
+
+  );
+};
+
+export default MealsSummary;
